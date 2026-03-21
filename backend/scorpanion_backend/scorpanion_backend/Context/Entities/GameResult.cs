8ce06e6e188389557e45a41b80382c52ab903fdd
Tao GRB <tao.grobois@proton.me>
2026-03-21 17:02:27 +0100

    Suppression backend console

--- backend/scorpanion_backend/scorpanion_backend/Context/Entities/GameResult.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace scorpanion_backend.Context.Entities;
-
-[Table("game_results")]
-public class GameResult :BaseEntity
-{
-    public required Game Game { get; set; }
-}
--- backend/scorpanion_backend/scorpanion_backend/Context/Entities/Scoreboard.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace scorpanion_backend.Context.Entities;
-
-[Table("scoreboards")]
-public class Scoreboard : BaseEntity
-{
-    // undefined
-}
