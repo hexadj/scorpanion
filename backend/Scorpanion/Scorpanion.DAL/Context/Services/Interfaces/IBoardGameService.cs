41680fce4fd128ac4015d3f33d8909a3993d464d
Tao GRB <tao.grobois@proton.me>
2026-03-22 10:09:47 +0100

    back: endpoint liste des jeux

--- backend/Scorpanion/Scorpanion.DAL/Context/Services/Interfaces/IBoardGameService.cs
+++ backend/Scorpanion/Scorpanion.DAL/Context/Services/Interfaces/IBoardGameService.cs
@@ -4,5 +4,14 @@ namespace Scorpanion.DAL.Context.Services.Interfaces;
 
 public interface IBoardGameService
 {
+    #region Commands
+
     Guid CreateBoardGame(BoardGameModel model);
+
+        #endregion
+
+    #region Queries
+
+    ICollection<BoardGameModel> GetAllBoardGames();
+    #endregion
 }
