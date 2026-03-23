c1a3d4b3bc41482dc562eedae287dc75bad8faec
Hex <a.jacquelot@gmail.com>
2026-03-23 18:54:18 +0100

    chore: update ESLint configuration and CI workflow

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -21,5 +21,9 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
   },
 ])
